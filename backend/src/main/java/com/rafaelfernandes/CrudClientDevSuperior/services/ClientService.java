e0c161dfa43657063a368da859d1dc7ab7db6c81
RafaelCoFernandes <rafaelfernandesxcvii@gmail.com>
2021-05-26 20:46:35 -0300

    Database Seeding

--- backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/services/ClientService.java
+++ backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/services/ClientService.java
@@ -2,15 +2,21 @@ package com.rafaelfernandes.CrudClientDevSuperior.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.rafaelfernandes.CrudClientDevSuperior.entities.Client;
 import com.rafaelfernandes.CrudClientDevSuperior.repositories.ClientRepository;
 
+@Service
 public class ClientService {
 
+	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Client> findAll() {
 		return repository.findAll();
 	}
-
 }
