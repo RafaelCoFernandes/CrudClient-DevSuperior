c978dfe117bd9194fa4d39b952984491167741d1
RafaelCoFernandes <rafaelfernandesxcvii@gmail.com>
2021-05-26 19:46:09 -0300

    Project Created

---
+++ backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/CrudClientDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelfernandes.CrudClientDevSuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientDevSuperiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaelfernandes/CrudClientDevSuperior/CrudClientDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelfernandes.CrudClientDevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
