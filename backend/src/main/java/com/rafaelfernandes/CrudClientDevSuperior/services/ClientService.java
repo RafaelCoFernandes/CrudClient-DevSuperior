fde9e5d478b4e8b716f801bc1391cbe5e1055f52
RafaelCoFernandes <rafaelfernandesxcvii@gmail.com>
2021-05-26 22:19:07 -0300

    Pagination

--- backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/services/ClientService.java
+++ backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/services/ClientService.java
@@ -1,10 +1,10 @@
 package com.rafaelfernandes.CrudClientDevSuperior.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,11 +20,11 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
