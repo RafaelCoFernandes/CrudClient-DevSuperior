9d5031ee4a959639de932359e996f3a9c5a68796
RafaelCoFernandes <rafaelfernandesxcvii@gmail.com>
2021-05-26 21:00:09 -0300

    DTO

--- backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/resources/ClientResource.java
+++ backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/resources/ClientResource.java
@@ -2,23 +2,25 @@ package com.rafaelfernandes.CrudClientDevSuperior.resources;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rafaelfernandes.CrudClientDevSuperior.entities.Client;
+import com.rafaelfernandes.CrudClientDevSuperior.dto.ClientDTO;
 import com.rafaelfernandes.CrudClientDevSuperior.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
 	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/services/ClientService.java
+++ backend/src/main/java/com/rafaelfernandes/CrudClientDevSuperior/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.rafaelfernandes.CrudClientDevSuperior.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rafaelfernandes.CrudClientDevSuperior.dto.ClientDTO;
 import com.rafaelfernandes.CrudClientDevSuperior.entities.Client;
 import com.rafaelfernandes.CrudClientDevSuperior.repositories.ClientRepository;
 
@@ -16,7 +18,8 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
